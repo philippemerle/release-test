57b3a92d274c2bd1454a51bdde8b532b52923a8f
Philippe Merle <Philippe.merle@inria.fr>
2017-04-12 11:41:37 +0200

    TravisCI

---
+++ tag_current_release.sh
@@ -0,0 +1,24 @@
+# Inspired from http://szagurskii.com/blog/how-to-deploy-apk-to-github-releases-with-travis-ci/
+
+# Are we on the right branch?
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+  
+  # Is this not a Pull Request?
+  if [ "$TRAVIS_PULL_REQUEST" = false ]; then
+    
+    # Is this not a build which was triggered by setting a new tag?
+    if [ -z "$TRAVIS_TAG" ]; then
+      echo -e "Starting to tag current release.\n"
+
+      git config --global user.email "travis@travis-ci.org"
+      git config --global user.name "Travis"
+
+      # Add tag and push to master.
+      git tag -f current -m "Tagged from TravisCI for build $TRAVIS_BUILD_NUMBER"
+      git push -f --tags origin # https://${GIT_TOKEN}@github.com/philippemerle/release-test
+      git fetch origin
+
+      echo -e "Done magic with tags.\n"
+  fi
+  fi
+fi
